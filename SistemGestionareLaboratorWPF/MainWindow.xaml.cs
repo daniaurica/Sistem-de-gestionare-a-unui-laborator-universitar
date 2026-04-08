0f9d232a28e43be9e530b77efd277c6f01626640
Daniel Aurica <danielaurica9@gmail.com>
2026-04-08 08:12:40 +0300

    Adaugare tema WPF si XAML Hot Reload

---
+++ SistemGestionareLaboratorWPF/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using Model.Entitati;
+using Model.Enumerari;
+
+namespace SistemGestionareLaboratorWPF
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Echipament echipament = new Echipament
+            {
+                Id = 1,
+                Denumire = "Laptop Dell",
+                Tip = TipEchipament.Laptop,
+                NumarTotal = 10,
+                NumarDisponibil = 7,
+                Optiuni = OptiuniEchipament.USB | OptiuniEchipament.HDMI | OptiuniEchipament.Wireless
+            };
+
+            txtId.Text = echipament.Id.ToString();
+            txtDenumire.Text = echipament.Denumire;
+            txtTip.Text = echipament.Tip.ToString();
+            txtNumarTotal.Text = echipament.NumarTotal.ToString();
+            txtNumarDisponibil.Text = echipament.NumarDisponibil.ToString();
+            txtOptiuni.Text = echipament.Optiuni.ToString();
+        }
+    }
+}
